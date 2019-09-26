b9b9400fcb08d64830c7f0e1adbfceb769aeeda7
Joao Salgado <joaosalgado@hotmail.com>
2019-09-26 13:47:56 +0100

    use linked list

--- src/main/java/tdd/Stack.java
+++ src/main/java/tdd/Stack.java
@@ -2,16 +2,14 @@ package tdd;
 
 public class Stack {
     private int size = 0;
-    private int[] values = new int[2];
+    private StackObject current;
 
     public boolean isEmpty() {
         return size == 0;
     }
 
     public void push(final int value) {
-        if (size == values.length)
-            this.values = copyArray(this.values);
-        this.values[size] = value;
+        this.current = new StackObject(value, this.current);
         size++;
     }
 
@@ -19,17 +17,22 @@ public class Stack {
         if (isEmpty())
             throw new IllegalStateException();
         size--;
-        return values[size];
+        int tmpVal = current.value;
+        this.current = this.current.next;
+        return tmpVal;
     }
 
     public int size() {
         return size;
     }
 
-    private int[] copyArray(int[] oldArray) {
-        int[] newValues = new int[oldArray.length * 2];
-        for (int i = 0; i < oldArray.length; i++)
-            newValues[i] = oldArray[i];
-        return newValues;
+    private class StackObject {
+        private int value;
+        private StackObject next;
+
+        public StackObject(final int value, final StackObject next) {
+            this.value = value;
+            this.next = next;
+        }
     }
 }
